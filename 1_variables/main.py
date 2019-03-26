8022e89a8c49110e3cfe8b5a2c270a76b8666a87
Ose Ogun <42394501+oseogun@users.noreply.github.com>
2019-03-25 21:52:34 -0600

    Add files via upload

---
+++ 1_variables/main.py
@@ -0,0 +1,70 @@
+"""
+ Pirple Python Assignment One
+ List of attributes of a song I like
+"""
+
+# Declare variables
+
+# Song Title
+title = "Sugar"
+
+# Name of Artist
+artist = "Maroon 5"
+
+# Song writer
+songWriter = "Adam Levin"
+
+# Available for
+digitalDownload = True
+
+# Album Title
+albumTitle = "V"
+
+# Song length
+length = "3mins 55secs"
+
+# Artist Label
+label = "222/Interscope"
+
+# Recording studio
+studio = "Conway Recording Studio"
+
+# Year of release
+releaseYear = 2014
+
+# Track position on the album
+trackNumber = 5
+
+# Song genre(s)
+genre = ["Disco","Pop-Funk","Soul"]
+
+# Song producer(s)
+producers = ["Ammo", "Cirkut"]
+
+"""
+ Print declared variables
+"""
+
+print("Song Title: " + title)
+
+print("Artist: " + artist)
+
+print("Writer: " + songWriter)
+
+print("Downlable?:", digitalDownload)
+
+print("Album Title: " + albumTitle)
+
+print("Length: " + length)
+
+print("Label: " + label)
+
+print("Studio: " + studio)
+
+print("Released in: ",  releaseYear)
+
+print("Track No.: ", trackNumber)
+
+print("Genre: ", genre)
+
+print("Producers: ", producers)
